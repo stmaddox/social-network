be56692044f745a267b338fd2475715f74b7a82f
Sean M <sean.maddox89@yahoo.com>
2022-09-08 02:55:39 -0700

    created server and user model

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+const { User } = require('.');
+
+const UserSchema = new Schema (
+    {
+        username: {
+            type: String,
+            unique: true,
+            require: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            require: true,
+            unique: true,
+            match: [/.+@.+\..+/],
+        },
+        thoughts: {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        },
+        friends: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+// get number of friends 
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
