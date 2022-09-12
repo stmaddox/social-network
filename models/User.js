a431988704be7d801d579e231d4b7d4b624ede2b
Sean M <sean.maddox89@yahoo.com>
2022-09-11 22:08:04 -0700

    added missing [] from User model. should be last push

--- models/User.js
+++ models/User.js
@@ -1,41 +1,45 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model, Types } = require("mongoose");
 
-const UserSchema = new Schema (
-    {
-        username: {
-            type: String,
-            unique: true,
-            require: true,
-            trim: true
-        },
-        email: {
-            type: String,
-            require: true,
-            unique: true,
-            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
-        },
-        thoughts: {
-            type: Schema.Types.ObjectId,
-            ref: 'Thought'
-        },
-        friends: {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      require: true,
+      trim: true,
     },
-    {
-        toJSON: {
-            virtuals: true
-        },
-        id: false
-    }
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
 );
 
-// get number of friends 
-UserSchema.virtual('friendCount').get(function() {
-    return this.friends.length;
+// get number of friends
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
-const User = model('User', UserSchema);
+const User = model("User", UserSchema);
 
 module.exports = User;
